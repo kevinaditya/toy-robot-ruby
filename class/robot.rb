b6b3e0777b3212485480a8ab14ec1a96635bc187
kevin <k_vin92@yahoo.com>
2016-06-14 22:27:44 +1000

    move left right

--- class/robot.rb
+++ class/robot.rb
@@ -3,7 +3,46 @@ class Robot
 
   def place(x, y, facing)
     @x = x
-    @y = y    
+    @y = y
     @facing = facing
+  end
+
+  def move
+    case @facing 
+    when 'NORTH'
+      @y = @y + 1
+	when 'EAST'
+	  @x = @x + 1
+    when 'SOUTH'
+      @y = @y - 1 
+    when 'WEST'
+      @x = @x - 1
+    end
+  end
+
+  def left
+    case @facing 
+    when 'NORTH'
+      @facing = 'WEST'
+    when 'EAST'
+      @facing = 'NORTH'
+    when 'SOUTH'
+      @facing = 'EAST'
+    when 'WEST'
+      @facing = 'SOUTH'
+    end
+  end
+
+  def right
+    case @facing 
+    when 'NORTH'
+      @facing = 'EAST'
+    when 'EAST'
+      @facing = 'SOUTH'
+    when 'SOUTH'
+      @facing = 'WEST'
+    when 'WEST'
+      @facing = 'NORTH'
+    end
   end
 end
