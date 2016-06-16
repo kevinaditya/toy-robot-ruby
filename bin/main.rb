15675f11e43c50d048b3b14a0ad9efce5f3d3dbd
kevin <k_vin92@yahoo.com>
2016-06-17 01:41:13 +1000

    Add sample test commands

--- bin/main.rb
+++ bin/main.rb
@@ -2,6 +2,49 @@ require_relative '../class/robot'
 require_relative '../class/command'
 
 table = Table.new
+
+puts "Sample test 1"
+robot1 = Robot.new(table)
+command = Command.new(robot1)
+puts "PLACE 0,0,NORTH"
+command.process("PLACE 0,0,NORTH")
+puts "MOVE"
+command.process("MOVE")
+puts "REPORT"
+command.process("REPORT")
+puts "\n"
+
+puts "Sample test 2"
+robot2 = Robot.new(table)
+command = Command.new(robot2)
+puts "PLACE 0,0,NORTH"
+command.process("PLACE 0,0,NORTH")
+puts "LEFT"
+command.process("LEFT")
+puts "REPORT"
+command.process("REPORT")
+puts "\n"
+
+puts "Sample test 3"
+robot3 = Robot.new(table)
+command = Command.new(robot3)
+puts "PLACE 1,2,EAST"
+command.process("PLACE 1,2,EAST")
+puts "MOVE"
+command.process("MOVE")
+puts "MOVE"
+command.process("MOVE")
+puts "LEFT"
+command.process("LEFT")
+puts "MOVE"
+command.process("MOVE")
+puts "REPORT"
+command.process("REPORT")
+puts "\n"
+
+#Now try it yourself
+puts "Now you can start with your own robot!"
+
 robot = Robot.new(table)
 command = Command.new(robot)
 
